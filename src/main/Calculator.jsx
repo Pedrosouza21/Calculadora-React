503ac1a5d773930e96aa73886df8a7cbdf0b5eff
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-12-02 11:58:27 -0300

    Display Component

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import './Calculator.css'
 
 import Button from "../components/Button";
+import Display from "../components/Display";
 
 export default class Calculator extends Component {
     render(){
         return (
             <div className="calculator">
+                <Display value={100} />
                 <Button  label="AC" />
                 <Button  label="/"  />
                 <Button  label="7"  />
@@ -16,8 +18,14 @@ export default class Calculator extends Component {
                 <Button  label="4"  />
                 <Button  label="5"  />
                 <Button  label="6"  />
+                <Button  label="-"  />
                 <Button  label="1"  />
-                <Button  label= "2"  />
+                <Button  label="2"  />
+                <Button  label="3"  />
+                <Button  label="+"  />
+                <Button  label="0"  />
+                <Button  label="."  />
+                <Button  label="="  />
                 
 
             </div>
